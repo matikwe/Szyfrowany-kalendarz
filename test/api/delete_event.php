0ea0e6269ab784488686c6935296b73b432e025d
matikwe <64837215+matikwe@users.noreply.github.com>
2021-06-05 12:51:04 +0200

    delete event

--- test/api/delete_event.php
+++ test/api/delete_event.php
@@ -8,6 +8,9 @@ $userId = $_SESSION['currID'];
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
+$data = new DB_connect('calendar');
 
-$data = "Hi".  $_POST['end'];
-echo json_encode($data); ?>
+$id = $_POST['id'];
+
+$deleteEvent = $data->getHandle()->query('DELETE FROM wydarzenia WHERE id="'.$id.'"');
+?>
